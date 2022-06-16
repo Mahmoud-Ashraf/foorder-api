82b1563253505d424978072b402852f759ceade3
Mahmoud taha <mahmoud.taha@idemia.com>
2022-06-16 15:06:11 +0200

    Enhance config code

--- controllers/configurations.js
+++ controllers/configurations.js
@@ -17,47 +17,6 @@ exports.getConfigs = (req, res, next) => {
 
 };
 
-// exports.addResturant = (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     const error = new Error('Validation Faild, Enter data in correct format');
-//     error.statusCode = 422;
-//     throw error;
-//     // res.status(422).json({
-//     //   message: "Validation Faild, Enter data in correct format",
-//     //   errors: errors.array(),
-//     // });
-//     // console.log('hello', res);
-//   }
-
-//   // const name = req.body.name;
-//   // const content = req.body.type;
-//   const resturant = new Resturant(req.body);
-//   // const resturant = new Resturant({
-//   //   name: name,
-//   //   content: content,
-//   //   vote: 0
-//   // });
-//   console.log(resturant);
-//   resturant.save()
-//     .then(resturant => {
-//       // let resturant = req.body;
-//       // resturant.id = new Date().toISOString();
-//       // Create Resturant in db
-//       // 201 status code means created in db
-//       // 200 status code means just success
-//       res.status(201).json({
-//         message: "Resturant added successfully",
-//         resturant: resturant,
-//       });
-//     }).catch(err => {
-//       if (!err.statusCode) {
-//         err.statusCode = 500;
-//       }
-//       next(err);
-//     })
-// };
-
 exports.updateConfigs = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
