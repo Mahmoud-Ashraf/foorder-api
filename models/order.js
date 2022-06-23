a79709da923095280986840e75418460fb26ca38
Mahmoud taha <mahmoud.taha@idemia.com>
2022-06-23 12:49:49 +0200

    change order Model

--- models/order.js
+++ models/order.js
@@ -31,7 +31,7 @@ const orderSchema = new Schema({
     },
     grandTotal: {
         type: Number,
-        default: 0
+        default: this.totalOrderPrice
     },
     status: {
         type: String,
